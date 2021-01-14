3d3844f6d4438046cbeeb2e49385e59c5fe6af5b
Shubham Kar <shubhamkar77@gmail.com>
2021-01-14 13:05:28 +0530

    Updated matrixMul.go

--- algorithms/LinearEquation/matrixMul.go
+++ algorithms/LinearEquation/matrixMul.go
@@ -8,20 +8,18 @@ import (
 
 func main() {
 	A := [][]int{
-		{1,2,3,4,5,6},
-		{2,3,4,5,6,7},
-		{3,4,5,6,7,8},
-		{4,5,6,7,8,9},
-		{5,6,7,8,9,10},
-		{6,7,8,9,10,11},
+		{1,2,3,4,5},
+		{2,3,4,5,6},
+		{3,4,5,6,7},
+		{4,5,6,7,8},
+		{5,6,7,8,9},
 	}
 	B := [][]int{
-		{1,2,3,4,5,6},
-		{2,3,4,5,6,7},
-		{3,4,5,6,7,8},
-		{4,5,6,7,8,9},
-		{5,6,7,8,9,10},
-		{6,7,8,9,10,11},
+		{1,2,3,4,5},
+		{2,3,4,5,6},
+		{3,4,5,6,7},
+		{4,5,6,7,8},
+		{5,6,7,8,9},
 	}
 	fmt.Println(len(A))
 	t1 := time.Now().Nanosecond()
@@ -36,36 +34,47 @@ func main() {
 	fmt.Println(t4-t3)
 }
 
-func matrixMultiplication(A, B [][]int) []int {
+func matrixMultiplication(A, B [][]int) [][]int {
 	size := len(A)
-	C := make([]int,size)
+	C := make([][]int,size)
 	var wg sync.WaitGroup
-	f := func(A, B [][]int, i int, size int) {	
-		j := i
-		C[i] = 0
-			
-		for k := 0; k<size; k++ {
-			C[i] = C[i] + A[i][k] * B[k][j]		
+    f := func(A, B [][]int, i int, size int) {	
+		for j := 0; j<size; j++ {
+		    C[i][j] = 0
+    		for k := 0; k<size; k++ {
+    			C[i][j] = C[i][j] + A[i][k] * B[k][j]		
+    		}
 		}
 		wg.Done()
 	}
 	
 	for i := 0; i<size; i++ {
 		wg.Add(1)
+		C[i] = make([]int, size)
 		go f(A, B, i, size)
+		/*go func(A, B [][]int, i int, size int) {	
+		    for j := 0; j<size; j++ {
+		        C[i][j] = 0
+		        for k := 0; k<size; k++ {
+			        C[i][j] = C[i][j] + A[i][k] * B[k][j]		
+		        }
+	        }
+	         wg.Done()
+	   }(A,B,i,size)*/
 	}
 	wg.Wait()
 	return C
 }
 
-func Multiply(A, B [][]int) []int {
+func Multiply(A, B [][]int) [][]int {
 	size := len(A)
-	C := make([]int,size)
+	C := make([][]int,size)
 	for i := 0; i<size; i++ {
+	    C[i] = make([]int,size)
 		for j := 0; j<size; j++ {
-			C[i] = 0
+			C[i][j] = 0
 			for k := 0; k<size; k++ {
-				C[i] = C[i] + A[i][k] * B[i][k]
+				C[i][j] = C[i][j] + A[i][k] * B[k][j]
 			}	
 		}
 	}
